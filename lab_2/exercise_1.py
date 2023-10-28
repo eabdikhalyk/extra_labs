a0cf71f73b7105e5fa7431b2f03b19e298bf7bb4
Yerkebulan A. <abdikhalykerkebulan@gmail.com>
2023-10-28 19:14:56 +0500

    lab 2

---
+++ lab_2/exercise_1.py
@@ -0,0 +1,11 @@
+# 1. Exercise
+import math
+
+a = float(input("Введите строну a: "))
+b = float(input("Введите строну b: "))
+
+if 1001 > a > 0 and 1001 > b > 0:
+    c = math.sqrt(a**2 + b**2)
+    print(c)
+else:
+    print("a или b ,больше 1000 или меньше 0" )
