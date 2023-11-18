585e189377957201c81ca99b53379a8fc3d686b4
eabdikhalyk <eabdikhalyk@mail.ru>
2023-11-19 05:44:16 +0600

    lab 3

--- lab_3/calculator.py
+++ lab_3/calculator.py
@@ -9,7 +9,7 @@ options='''Available options:
 '''
 a = 0.0
 b = 0.0
-is_on_go = True
+is_go_on = True
 
 def print_options():
     print(options)
@@ -25,27 +25,33 @@ def input_operators():
         except:
             print("You entered string, please enter number")
 
-
 def add():
     print("1. a + b")
     input_operators()
     result = a + b
     print(f"Answer a + b = {result}")
+
 def subtract():
     print("2. a - b")
     input_operators()
     result = a - b
     print(f"Answer a - b = {result}")
+
 def multiply():
     print("3. a * b")
     input_operators()
     result = a * b
     print(f"Answer a * b = {result}")
+
 def divide():
     print("4. a % b")
     input_operators()
-    result = a / b
-    print(f"Answer a % b = {result}")
+    try:
+        result = a / b
+        print(f"Answer a % b = {result}")
+    except ZeroDivisionError:
+        print("Zero Division Error")
+
 def fibonacci():
     print("5. Fibonacci number")
     fnum1 = 1
@@ -60,8 +66,7 @@ def fibonacci():
 
     print(f"Answer: {fnum2}")
 
-
-while is_on_go:
+while is_go_on:
     print_options()
     chosen_option = int(input('Enter option: '))
     if chosen_option == 1:
@@ -76,4 +81,4 @@ while is_on_go:
         fibonacci()
     elif chosen_option == 6:
         print("Good bye!")
-        is_on_go = False
+        is_go_on = False
